db8f9604bb8aea7a45ba9234f486216c853bb82b
Johnny <jakhongir12@users.noreply.github.com>
2022-07-03 03:03:43 +0200

    react app project:)

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import { Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import MainPage from "./components/mainPage/MainPage";
+import OurShirts from "./components/ourShirts/OurShirts";
+import Shop from "./components/shop/Shop";
+import ShopItem from "./components/shopItem/ShopItem";
+import History from "./components/history/History";
+import Card from "./components/card/card";
+import Checkout from "./pages/checkout/checkout";
+import EmptyCard from "./components/empty-card/emptyCard";
+import Vaucher from "./pages/vaucher/vaucher";
+import AboutPage from "./components/about/aboutPage";
+import HowToSelectCustomShirt from "./components/how-to-select-custom-shirt/how-to-select-custom-shirt";
+import ContactUs from "./components/contactUsPage/mainContactUs/contactUs";
+
+function App() {
+  return (
+    <div className="App">
+      <Router history={History}>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/ourShirts" component={OurShirts} />
+          <Route path="/shop" exact component={Shop} />
+          <Route path="/shopItem" exact component={ShopItem} />
+          <Route path="/card" exact component={Card} />
+          <Route path="/checkout" exact component={Checkout} />
+          <Route path="/emptyCard" exact component={EmptyCard} />
+          <Route path="/vaucher" exact component={Vaucher} />
+          <Route path="/about" exact component={AboutPage} />
+          <Route
+            path="/selectCustomShirt"
+            exact
+            component={HowToSelectCustomShirt}
+          />
+          <Route path="/contactus" exact component={ContactUs} />
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/history/History.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { createBrowserHistory as history} from 'history';
+
+export default history();
